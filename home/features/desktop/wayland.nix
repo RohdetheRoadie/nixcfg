c68069017caa0582c46a9436e0e95682f54e6b1d
Sean Rohde <seanmichaelrohde@gmail.com>
2026-07-21 22:24:07 +0900

    added desktop feature moduel

---
+++ home/features/desktop/wayland.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.features.desktop.wayland;
+in {
+  options.features.desktop.wayland.enable = mkEnableOption "wayland extra tools and config";
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      waypipe
+    ];
+  };
+}

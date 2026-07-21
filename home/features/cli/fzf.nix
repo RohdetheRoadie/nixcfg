8df1e9e4abec32fcbe6e2bda76973dc1cdfa3e85
Sean Rohde <seanmichaelrohde@gmail.com>
2026-07-21 22:41:16 +0900

    fzf feature renamed

--- home/features/cli/fzf.nix
+++ home/features/cli/fzf.nix
@@ -32,7 +32,7 @@ in {
         "--bind 'ctrl-/:toggle-preview'"
       ];
       defaultCommand = "fd --type f --exclude .git --follow --hidden";
-      changeDirWidgetCommand = "fd --type d --exclude .git --follow --hidden";
+      changeDirWidget.command = "fd --type d --exclude .git --follow --hidden";
     };
   };
 }

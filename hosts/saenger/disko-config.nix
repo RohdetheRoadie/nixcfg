0d2b26b04ed5c477d3276fbb2fd5e053ed2f06f6
Sean Rohde <seanmichaelrohde@gmail.com>
2026-08-22 13:53:29 +0900

    updated disko to match hardware-config; switch failed

--- hosts/saenger/disko-config.nix
+++ hosts/saenger/disko-config.nix
@@ -1,19 +1,65 @@
-## This one should work for NVME
+# This one should work for NVME
+{
+  disko.devices = {
+    disk = {
+      nixos = {
+        # SSD hardware identifier use /dev/sda for hdd
+        device = "/dev/nvme0n1"; #Enable for HDD/disable for VM
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            # boot = {
+            #   size = "1024M";
+            #   type = "EF02"; # for grub MBR
+            # };
+            #Try this out to make bootable (VM didn't like the other way)
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+            swap = {
+              size = "16.9G";
+              content = {
+                type = "swap";
+                randomEncryption = true; # secures swap on restart/disable if promlems
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
+# # This one works for VM
 # {
 #   disko.devices = {
 #     disk = {
 #       nixos = {
-#         # SSD hardware identifier use /dev/sda for hdd
-#         device = "/dev/nvme0n1"; #Enable for HDD/disable for VM
 #         type = "disk";
+#         device = "/dev/vda";
 #         content = {
 #           type = "gpt";
 #           partitions = {
-#             # boot = {
-#             #   size = "1024M";
-#             #   type = "EF02"; # for grub MBR
-#             # };
-#             #Try this out to make bootable (VM didn't like the other way)
+#             boot = {
+#               size = "1M";
+#               type = "EF02"; # for grub MBR
+#             };
 #             ESP = {
 #               size = "512M";
 #               type = "EF00";
@@ -21,7 +67,6 @@
 #                 type = "filesystem";
 #                 format = "vfat";
 #                 mountpoint = "/boot";
-#                 mountOptions = [ "umask=0077" ];
 #               };
 #             };
 #             root = {
@@ -38,41 +83,3 @@
 #     };
 #   };
 # }
-
-# This one works for VM
-{
-  disko.devices = {
-    disk = {
-      nixos = {
-        type = "disk";
-        device = "/dev/vda";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
-            ESP = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-}

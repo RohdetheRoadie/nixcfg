c61b0b629cac44b120ef732a74f23dd18c01fb7c
Sean Rohde <seanmichaelrohde@gmail.com>
2026-09-03 18:05:34 +0900

    reset disko-config to VM

--- hosts/saenger/disko-config.nix
+++ hosts/saenger/disko-config.nix
@@ -1,83 +1,45 @@
-# This one should work for NVME
-{
-  disko.devices = {
-    disk = {
-      nixos = {
-        # SSD hardware identifier use /dev/sda for hdd
-        device = "/dev/nvme0n1"; #Enable for HDD/disable for VM
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            # boot = {
-            #   size = "1024M";
-            #   type = "EF02"; # for grub MBR
-            # };
-            #Try this out to make bootable (VM didn't like the other way)
-            ESP = {
-              # size = "1G";
-              size = 1073741824;
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            root = {
-              # size = "100%";
-              size = 480920062464;
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-            swap = {
-              # size = "16.9G";
-              size = 18111926784;
-              content = {
-                type = "swap";
-                randomEncryption = true; # secures swap on restart/disable if promlems
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-}
-
-# # This one works for VM
+# # This one should work for NVME
 # {
 #   disko.devices = {
 #     disk = {
 #       nixos = {
+#         # SSD hardware identifier use /dev/sda for hdd
+#         device = "/dev/nvme0n1"; #Enable for HDD/disable for VM
 #         type = "disk";
-#         device = "/dev/vda";
 #         content = {
 #           type = "gpt";
 #           partitions = {
-#             boot = {
-#               size = "1M";
-#               type = "EF02"; # for grub MBR
-#             };
+#             # boot = {
+#             #   size = "1024M";
+#             #   type = "EF02"; # for grub MBR
+#             # };
+#             #Try this out to make bootable (VM didn't like the other way)
 #             ESP = {
-#               size = "512M";
+#               # size = "1G";
+#               size = 1073741824;
 #               type = "EF00";
 #               content = {
 #                 type = "filesystem";
 #                 format = "vfat";
 #                 mountpoint = "/boot";
+#                 mountOptions = [ "umask=0077" ];
 #               };
 #             };
 #             root = {
-#               size = "100%";
+#               # size = "100%";
+#               size = 480920062464;
 #               content = {
 #                 type = "filesystem";
 #                 format = "ext4";
 #                 mountpoint = "/";
+#               };
+#             };
+#             swap = {
+#               # size = "16.9G";
+#               size = 18111926784;
+#               content = {
+#                 type = "swap";
+#                 randomEncryption = true; # secures swap on restart/disable if promlems
 #               };
 #             };
 #           };
@@ -86,3 +48,41 @@
 #     };
 #   };
 # }
+
+# This one works for VM
+{
+  disko.devices = {
+    disk = {
+      nixos = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
